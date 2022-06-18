04cfa74acb1b135270860f40abbb0e6df9184482
Jayeskumar <52567903+Jayeskumar@users.noreply.github.com>
2022-06-18 05:14:32 +0000

    comit

---
+++ first_repeated_element_from_the_tail.java
@@ -0,0 +1,18 @@
+class first_repeated_element_from_the_tail {
+    static int firstRepeatedElement(int input1[]){
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int i=input1.length-1;i>0;i--){
+            if(input1[i]>0){
+                if(map.containsKey(input1[i])){
+                    return input1[i];
+                }
+                map.put(input1[i],1);
+            }
+            
+        }
+        if(map.isEmpty())
+        return 0;
+        return input1[0];
+    }
+    
+}
